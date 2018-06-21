8ba57a94354c7eb0033262ae2086a7812b82ffa1
alonb31 <40149040+alonb31@users.noreply.github.com>
2018-06-21 11:46:06 +0300

    רק שיניתי את הפארסר יש עדיין ארורים אבל זה יעבוד מהר מאוד

--- src/foundation/Parser.java
+++ src/foundation/Parser.java
@@ -1,4 +1,10 @@
 package foundation;
+import blocks.Block;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Parser {
     private static Parser ourInstance = new Parser();
@@ -9,4 +15,34 @@ public class Parser {
 
     private Parser() {
     }
+
+    public Checkable parse(String filename) {
+        Block mainBlock = new Block();
+        try {
+            File file = new File(filename);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            this.parseBlock(bufferedReader, mainBlock);
+            fileReader.close();
+            return mainBlock;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    private int parseBlock(BufferedReader bufferedReader, Block block) throws IOException {
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            if(this.lineAction(line,block)){
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    private boolean lineAction(String line,Block block){ }
 }
+
+//TODO finidh this
