7818fca59e09be3653c0020629c29dd57ff71ddb
alonb31 <40149040+alonb31@users.noreply.github.com>
2018-06-25 13:58:52 +0300

    פונקציות !!!!!!!!!

--- src/foundation/Exceptions/SyntaxException.java
+++ src/foundation/Exceptions/SyntaxException.java
@@ -2,6 +2,6 @@ package foundation.Exceptions;
 
 public class SyntaxException  extends Exception {
     public SyntaxException(int rowNumber) {
-        super("The code is illegal.  The syntax error was in row" + String.valueOf(rowNumber));
+        super("The code is illegal. The syntax error was in row" + String.valueOf(rowNumber));
     }
 }
